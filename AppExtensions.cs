c8554292d8d3845aba021a1a8ffbc304d6382505
Andre Herman <amelco.herman@gmail.com>
2025-11-17 19:17:45 -0300

    factor out creation of tables at initialiation as an extension method

---
+++ AppExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blog
+{
+    public static class AppExtensions
+    {
+        public static void CreateTables(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.EnsureCreated();
+
+                dbContext.Database.ExecuteSqlRaw(@"
+                    CREATE TABLE IF NOT EXISTS BlogPosts (
+                        Id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Title   TEXT    NOT NULL,
+                        Content TEXT    NOT NULL
+                    );
+                ");
+
+                dbContext.Database.ExecuteSqlRaw(@"
+                    CREATE TABLE IF NOT EXISTS Comments (
+                        Id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                        BlogPostId  INTEGER NOT NULL,
+                        Content     TEXT    NOT NULL,
+                        FOREIGN KEY (BlogPostId) REFERENCES BlogPosts(Id)
+                    );
+                ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,14 +8,11 @@ namespace blog
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -29,7 +26,6 @@ namespace blog
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -40,31 +36,9 @@ namespace blog
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                dbContext.Database.EnsureCreated();
-
-                await dbContext.Database.ExecuteSqlRawAsync(@"
-                    CREATE TABLE IF NOT EXISTS BlogPosts (
-                        Id      INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Title   TEXT    NOT NULL,
-                        Content TEXT    NOT NULL
-                    );
-                ");
-
-                await dbContext.Database.ExecuteSqlRawAsync(@"
-                    CREATE TABLE IF NOT EXISTS Comments (
-                        Id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                        BlogPostId  INTEGER NOT NULL,
-                        Content     TEXT    NOT NULL,
-                        FOREIGN KEY (BlogPostId) REFERENCES BlogPosts(Id)
-                    );
-                ");
-            }
+            app.CreateTables();
 
             app.Run();
         }
